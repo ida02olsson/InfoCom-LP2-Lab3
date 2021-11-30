0dc07187b908bf9ea5b1bea4bc1d15fd7e4a2464
ida02olsson <90462101+ida02olsson@users.noreply.github.com>
2021-11-30 17:03:29 +0100

    Lab3

--- pi/pi_controller.py
+++ pi/pi_controller.py
@@ -2,35 +2,48 @@ import math
 import requests
 import argparse
 
+
 #Write you own function that moves the dron from one place to another 
 #the function returns the drone's current location while moving
 #====================================================================================================
-def your_function():
-    longitude = 13.21008
-    latitude = 55.71106
+def your_function(current, destination):
+    #print(current)
+
+    if (destination[0] - current[0]) > 0:
+        default_long = 1/10000
+    else: 
+        default_long = -1/10000
+
+    if (destination[1] - current[1]) > 0:
+        default_la = 1/10000
+    else: 
+        default_la = -1/10000
+
+    #print(default_la, default_long)
+
+ 
+    longitude = float(current[0] + default_long)
+    latitude = float(current[1] + default_la)
     
-    while current_coords != from_coords
-            if drone_coords[0] > from_coords[0] then drone_coords[0] -= 0.0001
-                elif drone_coords[0] < from_coords[0] then drone_coords[0] += 0.0001
-                else: drone_coords[0] += 0
-              
-            if drone_coords[1] > from_coords[1] then drone_coords[1] -= 0.0001
-                elif drone_coords[1] < from_coords[1] then drone_coords[1] += 0.0001
-                else: drone_coords[1] += 0
-                
-          while current_coords != to_coords
-            if drone_coords[0] > to_coords[0] then drone_coords[0] -= 0.0001
-                elif drone_coords[0] < to_coords[0] then drone_coords[0] += 0.0001
-                else: drone_coords[0] += 0
-              
-            if drone_coords[1] > to_coords[1] then drone_coords[1] -= 0.0001
-                elif drone_coords[1] < to_coords[1] then drone_coords[1] += 0.0001
-                else: drone_coords[1] += 0
-                              
-          return drone_location
+    if abs(longitude - destination[0]) < 1/1000:
+        longitude = destination[0]
+        
+    if abs(latitude - destination[1]) < 1/1000:
+        latitude = destination[1]
     
+    print(longitude, latitude)
+    
+  
+   # longitude = destination[0]
+    #latitude = destination[1]
+
     return (longitude, latitude)
 #====================================================================================================
+
+
+#(13.21003993027314, 55.71115995) current
+#(13.18293993027314, 55.71775995) from
+#(13.1876027, 55.7058176)         to
 
 
 def run(current_coords, from_coords, to_coords, SERVER_URL):
@@ -39,15 +52,23 @@ def run(current_coords, from_coords, to_coords, SERVER_URL):
     # 2. Plan a path with your own function, so that the drone moves from [current_address] to [from_address], and the from [from_address] to [to_address]. 
     # 3. While moving, the drone keeps sending it's location to the database.
     #====================================================================================================
-    while True:
-        drone_coords = your_function()
+    drone_coords = current_coords
+    while drone_coords != from_coords:
+        drone_coords = your_function(drone_coords, from_coords)
         with requests.Session() as session:
             drone_location = {'longitude': drone_coords[0],
                               'latitude': drone_coords[1]
-                       
-                             }
-            
+                        }
             resp = session.post(SERVER_URL, json=drone_location)
+
+
+    while drone_coords != to_coords:
+            drone_coords = your_function(drone_coords, to_coords)
+            with requests.Session() as session:
+                drone_location = {'longitude': drone_coords[0],
+                                'latitude': drone_coords[1]
+                            }
+                resp = session.post(SERVER_URL, json=drone_location)
   #====================================================================================================
 
    
@@ -72,3 +93,4 @@ if __name__ == "__main__":
     print(to_coords)
 
     run(current_coords, from_coords, to_coords, SERVER_URL)
+
